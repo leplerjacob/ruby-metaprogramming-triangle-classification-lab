a2076da2b24ec2867c4e9b27217472f3081db919
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-22 21:22:59 -0500

    commit tree 807e7db92899bc359a2452a6fd872e2be4367ca3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+
 class Triangle
-  # write code here
+  attr_accessor :x,:y,:z
+  def initialize(x,y,z)
+    @x = x
+    @y = y
+    @z = z
+    @kind = nil
+  end
+  
+  def kind
+    p sorted = [@x,@y,@z].sort
+    if @x + @y < @z or x + z < y or y + z > x and @x > 0 and @y > 0 and @z > 0 and sorted[0] + sorted[1] > sorted[2]
+      if @x == @y && @y == @z
+        @kind = :equilateral
+      elsif @x == @y or @y == @z or @z == @x
+        @kind = :isosceles
+      else
+        @kind = :scalene
+      end
+      @kind
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    "Not a triangle"
+  end
+
 end
